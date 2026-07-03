01f326c23d32269e62a5862168bc4409b7063783
yashprakash741-source <prakashyash741@gmail.com>
2026-07-03 12:48:59 +0000

    datatype

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,31 @@
+"use strict";//treat all JS code as newer version
+
+// alert(3+3) //we are using nodejs , not browser
+
+console.log(3
+    +3   // code readability should be high
+)
+
+
+let name = "yash"
+let age = 18
+let isloggedin = false
+
+
+//
+// number => 2 to power 15
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique 
+
+
+
+//object 
+
+console.log(typeof age)//number
+console.log(typeof undefined) // undefined
+console.log(typeof null) // ** object (datatype)
+
