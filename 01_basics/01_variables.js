16f0bcaf71cbc83daaf994c7554705471124434a
yashprakash741-source <prakashyash741@gmail.com>
2026-07-03 12:46:59 +0000

    variables and datatype

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 1234
+let accountEmail = "yashemail.com"
+var accountPassword="1384"
+accountCity = "Jaipur"
+
+//accountId= 2344// not allowed
+
+accountEmail="shdja@gma.com"
+accountPassword="34555"
+accountCity="delhi"
+
+/*prefer not to use var
+ because issue in block scope and functional scope  
+*/
+
+console.table([accountId,accountEmail,accountPassword,accountCity])
