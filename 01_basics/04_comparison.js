b21270fbc08fe6fe81919c78a9e03210ad690f48
yashprakash741-source <prakashyash741@gmail.com>
2026-07-04 12:22:54 +0000

    datatype summary

--- 01_basics/04_comparison.js
+++ 01_basics/04_comparison.js
@@ -1,14 +1,14 @@
-console.log( 2 > 1);
-console.log(2 >= 1);
-console.log(2 < 1);
-console.log(2 == 1);
-console.log(2 != 1);
+// console.log( 2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
 
 
 // console.log("2" > 1);
 // console.log("02" > 1);
 
-console.log(null > 0);
+console.log(null > 0);  // ese conversion ko avoid karna chaiye 
 console.log(null == 0);
 console.log(null >= 0);
 
---
+++ 01_basics/datatype-summary.js
@@ -0,0 +1,36 @@
+// #primitive datatype 
+
+//7 types : String , Number , boolean , null , undefined, Symbol BigInt 
+
+// const score = 100   
+// const scoreValue = 100.3
+
+// const isLoggedIn = False 
+// const outsideTemp = null
+let userEmail;
+
+const id= symbol('123')
+const anotherId = symbol('123')
+
+//console.log(id == anotherId);
+
+const bigNumber = 2443945803n
+
+
+
+//Refrence or Non Primitive
+
+// Array , Object , Function 
+
+const heroes = ["shaktiman" , "spiderman" ,"dogs"]
+let myObj = {
+    namr: "yash",
+    age: 32
+}
+
+const myFunction = function(){
+   //console.log("HEllo World");
+}
+
+console.log(typeof bigNumber )
+// https://262.ecma-international.org/5.1/#sec-11.4.3
