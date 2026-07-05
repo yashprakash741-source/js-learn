a67fc0bd36b73965b06516e028eaa45ec63863d7
yashprakash741-source <prakashyash741@gmail.com>
2026-07-05 13:04:31 +0000

    updated

--- 02_basics/02_arrays.js
+++ 02_basics/02_arrays.js
@@ -1,21 +1,22 @@
 const marvel_heros = ["thor", "Ironman", "spiderman"]
 const dc_heros = ["superman", "flash", "batman"]
 
-// marvel_heros.push(dc_heros)
+// marvel_heros.push(dc_heros)  ye array ko 4th index pe add karega 
 
 // console.log(marvel_heros);
 // console.log(marvel_heros[3][1]);
 
-// const allHeros = marvel_heros.concat(dc_heros)
+// const allHeros = marvel_heros.concat(dc_heros) ye array add kardega new array mein  
 // console.log(allHeros);
 
-const all_new_heros = [...marvel_heros, ...dc_heros]
+// spread operator ye bhi add karta h 
+const all_new_heros = [...marvel_heros, ...dc_heros] 
 
 // console.log(all_new_heros);
 
 const another_array = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]]
 
-const real_another_array = another_array.flat(Infinity)
+const real_another_array = another_array.flat(Infinity) // sari array ko ek me kardega
 console.log(real_another_array);
 
 
