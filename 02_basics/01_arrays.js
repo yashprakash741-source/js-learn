1c2b259367aaa9b1e3bbed49f5d5a4dd1c81da03
yashprakash741-source <prakashyash741@gmail.com>
2026-07-07 12:42:10 +0000

    updated arrays

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -32,8 +32,8 @@ const myArr2 = new Array(1, 2, 3, 4)
 console.log("A ", myArr);
 
 const myn1 = myArr.slice(1, 3)
-
 console.log(myn1);
+
 console.log("B ", myArr);
 
 
--- 02_basics/02_arrays.js
+++ 02_basics/02_arrays.js
@@ -3,10 +3,11 @@ const dc_heros = ["superman", "flash", "batman"]
 
 // marvel_heros.push(dc_heros)  ye array ko 4th index pe add karega 
 
-// console.log(marvel_heros);
-// console.log(marvel_heros[3][1]);
+console.log(marvel_heros);
+console.log(marvel_heros[3][1]);
 
-// const allHeros = marvel_heros.concat(dc_heros) ye array add kardega new array mein  
+// const allHeros = marvel_heros.concat(dc_heros) 
+//  ye array add kardega new array mein  
 // console.log(allHeros);
 
 // spread operator ye bhi add karta h 
@@ -17,16 +18,16 @@ const all_new_heros = [...marvel_heros, ...dc_heros]
 const another_array = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]]
 
 const real_another_array = another_array.flat(Infinity) // sari array ko ek me kardega
-console.log(real_another_array);
+// console.log(real_another_array);
 
 
 
-console.log(Array.isArray("Hitesh"))
-console.log(Array.from("Hitesh"))
-console.log(Array.from({name: "hitesh"})) // interesting
+// console.log(Array.isArray("Hitesh"))
+// console.log(Array.from("Hitesh"))
+// console.log(Array.from({name: "hitesh"})) // interesting
 
 let score1 = 100
 let score2 = 200
 let score3 = 300
 
-console.log(Array.of(score1, score2, score3));
+console.log(Array.of(score1, score2, score3)),
